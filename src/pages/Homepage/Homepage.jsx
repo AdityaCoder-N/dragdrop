a4355151b1f2646cafea658e058bcf839b894017
AdityaCoder-N <91337266+AdityaCoder-N@users.noreply.github.com>
2023-05-27 01:09:39 +0530

    Added homepage and routing

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Homepage.css'
+import { useNavigate } from 'react-router-dom'
+const Homepage = () => {
+
+    const navigate = useNavigate();
+
+  return (
+    <div className='homepage'>
+        <div className='homepage-container'>
+            <div className='heading'>
+                Welcome to GameLearn !
+            </div>
+            <div className='sub-txt'>
+                A place created specially to Learn while Playing.
+            </div>
+            <div className='home-play-btn' onClick={()=>{ navigate('/play') }}>
+                <i class="fa-solid fa-play"></i>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ src/pages/PlayScreen/PlayScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './PlayScreen.css'
+const PlayScreen = () => {
+  return (
+    <div className='playscreen'>
+        
+        <div>Exercise : <strong>Sort-Up</strong> </div>
+        <div><strong>Task - </strong>Arrange the numbers in Ascending Order.</div>
+
+
+
+    </div>
+  )
+}
+
+export default PlayScreen
